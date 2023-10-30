1345a279b80672ccad40586321057bec0005ecea
Sldark23 <115877460+Sldark23@users.noreply.github.com>
2023-10-30 19:33:31 -0300

    Create login_usuario.php

---
+++ login_usuario.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+if (strlen($email) > 3 && strlen($senha) > 3) {
+  $senha_cripto = md5($senha);
+
+  $conn = mysqli_connect("localhost", "root", "", "sistema");
+
+  //Execução da instrução SQL
+  $resultado_consulta = $conn->query("SELECT * from usuarios where email = '$email' AND senha = '$senha_cripto'");
+
+  //$usuarios recebe a lista de usuarios
+  $usuarios = mysqli_fetch_assoc($resultado_consulta);
+  
+  $_SESSION['nome'] = $usuarios["nome"];
+  $_SESSION['imagem'] = $usuarios["imagem"];
+  $_SESSION['email'] = $usuarios["email"];
+  $_SESSION['id'] = $usuarios["id"];
+
+  header('Location: home.php');
+
+}
+else {
+  echo "
+      <script>
+          alert('E-mail ou senha inválidos!')
+          location.href = 'index.php'
+      </script>
+";
+}
