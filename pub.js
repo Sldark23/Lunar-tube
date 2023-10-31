6f1b084a233401be784f6756256f02ff0be1082e
Sldark23 <115877460+Sldark23@users.noreply.github.com>
2023-10-31 00:12:45 +0000

    Up

---
+++ pub.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const { google } = require('googleapis');
+
+const apiKey = 'AIzaSyBQlxu6qe2cq3HjkL2larNuH--8IgS6zjQ'; // Substitua com sua chave de API do YouTube
+const youtube = google.youtube({
+  version: 'v3',
+  auth: apiKey,
+});
+
+async function fetchYouTubeVideos() {
+  try {
+    const response = await youtube.search.list({
+      part: 'snippet',
+      maxResults: 89, // Ajuste o número de resultados desejado
+      q: 'bloxfruits', // Substitua 'termo_de_pesquisa' pelo que deseja pesquisar
+    });
+
+    const videos = response.data.items.map((video) => ({
+      title: video.snippet.title,
+      embedURL: `https://www.youtube.com/embed/${video.id.videoId}`,
+    }));
+
+    console.log('Vídeos encontrados:');
+    videos.forEach((video, index) => {
+      console.log(`#${index + 1} - Título: ${video.title}, URL: ${video.embedURL}`);
+    });
+
+    const outputFile = 'videos.json';
+    fs.writeFileSync(outputFile, JSON.stringify({ videos }, null, 2));
+    console.log(`Vídeos salvos em ${outputFile}.`);
+  } catch (error) {
+    console.error('Erro ao buscar vídeos do YouTube:', error);
+  }
+}
+
+fetchYouTubeVideos();
--- sh.sh
+++ sh.sh
@@ -1,5 +1,2 @@
-mv cadastro_usuario.php public
-mv cadastro.php public
-mv index.php public
-mv login_usuario.php public
-mv home.php public
+echo "pesquisafo"
+node pub.js
